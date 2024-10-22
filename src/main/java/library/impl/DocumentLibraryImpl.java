9b5a3190545ce6e6c23e3a848bf932a2f7bf4cb4
dkwelcher <dkwelcher@gmail.com>
2024-10-22 17:10:30 -0400

    Added count field & constraints on addText()
    To avoid long processing times with the pattern randomizer, the document count has been limited to 3.

--- src/main/java/library/impl/DocumentLibraryImpl.java
+++ src/main/java/library/impl/DocumentLibraryImpl.java
@@ -1,16 +1,34 @@
-package library;
+package library.impl;
+
+import library.DocumentLibrary;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DocumentLibrary {
-    private final List<String> listOfDocumentsAsStrings = new ArrayList<>();
+public class DocumentLibraryImpl implements DocumentLibrary {
+    private static final String REGEX = "(?<!\\w\\.\\w.)(?<![A-Z][a-z]\\.)(?<=[.!?])\\s";
+    private int count;
+    private final List<String[]> listOfDocumentsAsStrings;
 
-    public void addText(String text) {
-        this.listOfDocumentsAsStrings.add(text);
+    public DocumentLibraryImpl() {
+        this.count = 0;
+        this.listOfDocumentsAsStrings = new ArrayList<>();
     }
 
-    public List<String> getListOfDocumentsAsStrings() {
+    public int getCount() {
+        return this.count;
+    }
+
+    public List<String[]> getListOfDocumentsAsStrings() {
         return this.listOfDocumentsAsStrings;
+    }
+
+    public boolean addText(String text) {
+        if (count >= 3) {
+            return false;
+        }
+        count++;
+        String[] splitText = text.split(REGEX);
+        return this.listOfDocumentsAsStrings.add(splitText);
     }
 }
