bcf7b28cfd11aef395cc4308d1e8bb9fafd55c84
dkwelcher <dkwelcher@gmail.com>
2024-10-22 17:13:46 -0400

    Implemented CombinationWriter inteface

---
+++ src/main/java/randomizer/impl/CombinationWriterImpl.java
@@ -0,0 +1,26 @@
+package randomizer.impl;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class CombinationWriterImpl {
+    private final List<String> combinations;
+
+    public CombinationWriterImpl(final List<String> combinations) {
+        this.combinations = combinations;
+    }
+
+    public boolean writeCombinationsToFile(String filePath) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath))) {
+            for (String combination : combinations) {
+                bufferedWriter.write(combination);
+                bufferedWriter.newLine();
+            }
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+}
