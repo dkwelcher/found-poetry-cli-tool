023da30f07e40bc81f7a88c5a49d0a3bb19fa208
dkwelcher <dkwelcher@gmail.com>
2024-10-22 21:06:51 -0400

    Added class for instantiating PosTagger & handling exceptions

---
+++ src/main/java/tagger/PosTaggerManager.java
@@ -0,0 +1,38 @@
+package tagger;
+
+import exception.IncorrectFileFormatException;
+import exception.NonExistentFileException;
+import exception.PosTaggerIOException;
+import tagger.impl.PosTaggerImpl;
+
+public class PosTaggerManager {
+    private final String filePath;
+
+    public PosTaggerManager(final String filePath) {
+        this.filePath = filePath;
+    }
+
+    public PosTaggerResult getResult() {
+        return getPosTaggerResult();
+    }
+
+    private PosTaggerResult getPosTaggerResult() {
+        final String systemShutdownMessage = "System forced to close.";
+        final String noErrorMessage = "No errors. PosTagger is ready.";
+        final String incorrectFileFormatExceptionMessage = "Incorrect file format: " + this.filePath + ". Expected format: .bin; " + systemShutdownMessage;
+        final String nonExistentFileExceptionMessage = "File does not exist: " + this.filePath + "; " + systemShutdownMessage;
+        final String posTaggerIOExceptionMessage = "I/O error occurred while loading the file: " + this.filePath + "; " + systemShutdownMessage;
+
+        PosTagger posTagger;
+        try {
+            posTagger = new PosTaggerImpl(this.filePath);
+            return new PosTaggerResult(posTagger, noErrorMessage);
+        } catch (IncorrectFileFormatException e) {
+            return new PosTaggerResult(null, incorrectFileFormatExceptionMessage);
+        } catch (NonExistentFileException e) {
+            return new PosTaggerResult(null, nonExistentFileExceptionMessage);
+        } catch (PosTaggerIOException e) {
+            return new PosTaggerResult(null, posTaggerIOExceptionMessage);
+        }
+    }
+}
