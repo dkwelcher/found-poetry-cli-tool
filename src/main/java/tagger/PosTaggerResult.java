a556c6ffdf4b89c0b08f1959277c601b85466575
dkwelcher <dkwelcher@gmail.com>
2024-10-22 20:53:25 -0400

    Renamed class
    The Manager suffix doesn't fit the behavior of the class since it's a record.

--- src/main/java/tagger/PosTaggerResult.java
+++ src/main/java/tagger/PosTaggerResult.java
@@ -1,6 +1,6 @@
 package tagger;
 
-public record PosTaggerManager(PosTagger posTagger, String errorMessage) {
+public record PosTaggerResult(PosTagger posTagger, String errorMessage) {
 
     public boolean isSuccess() {
         return this.posTagger != null;
