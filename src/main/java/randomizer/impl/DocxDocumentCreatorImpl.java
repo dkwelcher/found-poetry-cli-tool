538c73b0ca58d2e97b8af4f19d2d197d3e9a9164
dkwelcher <dkwelcher@gmail.com>
2024-10-24 10:13:27 -0400

    Add impl for creating .docx

---
+++ src/main/java/randomizer/impl/DocxDocumentCreatorImpl.java
@@ -0,0 +1,30 @@
+package randomizer.impl;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import randomizer.DocxDocumentCreator;
+
+import java.util.List;
+
+public class DocxDocumentCreatorImpl implements DocxDocumentCreator {
+    private List<String> combinations;
+    @Override
+    public void setCombinations(List<String> combinations) {
+        this.combinations = combinations;
+    }
+
+    @Override
+    public XWPFDocument create() {
+        XWPFDocument document = new XWPFDocument();
+
+        XWPFParagraph paragraph = document.createParagraph();
+        XWPFRun run = paragraph.createRun();
+
+        for (String combination : combinations) {
+            run.setText(combination);
+            run.addBreak();
+        }
+        return document;
+    }
+}
