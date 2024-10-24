fd6676ebcb28573868c3c2f350d1cfa755d89475
dkwelcher <dkwelcher@gmail.com>
2024-10-24 16:38:19 -0400

    Modified according to requirements change
    Implementation of DocumentLibrary aligns with new specifications of interface.

--- src/main/java/library/impl/DocumentLibraryImpl.java
+++ src/main/java/library/impl/DocumentLibraryImpl.java
@@ -4,22 +4,46 @@ import library.DocumentLibrary;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DocumentLibraryImpl implements DocumentLibrary {
-    private static final String REGEX = "(?<!\\w\\.\\w.)(?<![A-Z][a-z]\\.)(?<=[.!?])\\s";
-    private final List<String[]> listOfDocumentsAsStrings;
+    private static final String SENTENCE_REGEX = "(?<!\\w\\.\\w.)(?<![A-Z][a-z]\\.)(?<=[.!?])\\s";
+    private static final String TOKEN_REGEX = "\\w[\\w'-]*|[.,!?;]";
+    private final List<String> listOfDocuments;
 
     public DocumentLibraryImpl() {
-        this.listOfDocumentsAsStrings = new ArrayList<>();
+        this.listOfDocuments = new ArrayList<>();
     }
 
-
-    public List<String[]> getListOfDocumentsAsStrings() {
-        return this.listOfDocumentsAsStrings;
+    @Override
+    public void add(String text) {
+        this.listOfDocuments.add(text);
     }
 
-    public void addText(String text) {
-        String[] splitText = text.split(REGEX);
-        this.listOfDocumentsAsStrings.add(splitText);
+    @Override
+    public List<String[]> getListOfDocumentsAsSentences() {
+        List<String[]> listOfDocumentsAsSentences = new ArrayList<>();
+        for (String document : this.listOfDocuments) {
+            listOfDocumentsAsSentences.add(document.split(SENTENCE_REGEX));
+        }
+        return listOfDocumentsAsSentences;
+    }
+
+    @Override
+    public List<String[]> getListOfDocumentsAsTokens() {
+        List<String[]> listOfDocumentsAsTokens = new ArrayList<>();
+        Pattern tokenPattern = Pattern.compile(TOKEN_REGEX);
+
+        for (String document : this.listOfDocuments) {
+            List<String> tokensList = new ArrayList<>();
+            Matcher matcher = tokenPattern.matcher(document);
+            while (matcher.find()) {
+                tokensList.add(matcher.group());
+            }
+            listOfDocumentsAsTokens.add(tokensList.toArray(new String[0]));
+        }
+
+        return listOfDocumentsAsTokens;
     }
 }
