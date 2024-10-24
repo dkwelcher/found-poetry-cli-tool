45d03082c68bee3f54a3b3974005425c33f80351
dkwelcher <dkwelcher@gmail.com>
2024-10-24 16:41:16 -0400

    Added interface for generating combos accord.to pattern

---
+++ src/main/java/randomizer/PatternRandomizer.java
@@ -0,0 +1,10 @@
+package randomizer;
+
+import java.util.List;
+import java.util.Map;
+
+public interface PatternRandomizer {
+    void setTagsToTokens(Map<String, List<String>> tagsToTokens);
+    void setTagClasses(String[] tagClasses);
+    List<String> generateCombinations();
+}
