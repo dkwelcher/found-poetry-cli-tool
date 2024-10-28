dc182cff981282098af24b944c9432ea310ff422
dkwelcher <dkwelcher@gmail.com>
2024-10-28 17:28:25 -0400

    Added initializeTagger method & throws logic
    PosTagger exceptions condensed to reduce responsibility of class. Exceptions resulting from human error moved to calling class.

--- src/main/java/model/tagger/impl/PosTaggerImpl.java
+++ src/main/java/model/tagger/impl/PosTaggerImpl.java
@@ -1,9 +1,6 @@
 package model.tagger.impl;
 
-import exception.IncorrectFileFormatException;
-import exception.NonExistentFileException;
-import exception.NullOrEmptyFilePathException;
-import exception.PosTaggerIOException;
+import exception.*;
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSTaggerME;
 import model.tagger.PosTagger;
@@ -11,33 +8,12 @@ import model.tagger.PosTagger;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class PosTaggerImpl implements PosTagger {
     private POSTaggerME tagger;
 
-    public PosTaggerImpl(String filePath) throws NullOrEmptyFilePathException, IncorrectFileFormatException, NonExistentFileException, PosTaggerIOException {
-        validateFilePath(filePath);
-        initializeTagger(filePath);
-    }
-
-    private void validateFilePath(String filePath) throws NullOrEmptyFilePathException, IncorrectFileFormatException, NonExistentFileException {
-        if (filePath == null || filePath.trim().isEmpty()) {
-            throw new NullOrEmptyFilePathException((filePath));
-        }
-
-        final String expectedFormat = ".bin";
-        if (!filePath.endsWith(expectedFormat)) {
-            throw new IncorrectFileFormatException(filePath, expectedFormat);
-        }
-
-        if (!Files.exists(Paths.get(filePath))) {
-            throw new NonExistentFileException(filePath);
-        }
-    }
-
-    private void initializeTagger(String filePath) throws PosTaggerIOException {
+    @Override
+    public void initializeTagger(String filePath) throws PosTaggerIOException {
         POSModel model;
         try (InputStream modelIn = new FileInputStream(filePath)) {
             model = new POSModel(modelIn);
@@ -47,7 +23,11 @@ public class PosTaggerImpl implements PosTagger {
         this.tagger = new POSTaggerME(model);
     }
 
+    @Override
     public String[] getTags(String[] tokens) {
+        if (this.tagger == null) {
+            throw new TaggerNotInitializedException();
+        }
         return this.tagger.tag(tokens);
     }
 }
