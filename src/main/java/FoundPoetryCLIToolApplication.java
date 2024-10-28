c9d67b965bb99c7b38fe3ed3fc4899ea629d3a97
dkwelcher <dkwelcher@gmail.com>
2024-10-28 17:25:43 -0400

    Moved validation / exception handling for PosTagger to main
    Former PosTaggerManager.java & PosTaggerResult.java deleted to reduce class coupling.

--- src/main/java/FoundPoetryCLIToolApplication.java
+++ src/main/java/FoundPoetryCLIToolApplication.java
@@ -1,5 +1,9 @@
 import application.Application;
 import config.ModelFilePathConfig;
+import exception.IncorrectFileFormatException;
+import exception.NonExistentFileException;
+import exception.NullOrEmptyFilePathException;
+import exception.PosTaggerIOException;
 import model.library.DocumentLibrary;
 import model.library.impl.DocumentLibraryImpl;
 import model.randomizer.DocxCombinationWriter;
@@ -10,18 +14,32 @@ import model.randomizer.impl.DocxCombinationWriterImpl;
 import model.randomizer.impl.DocxDocumentCreatorImpl;
 import model.randomizer.impl.PatternRandomizerImpl;
 import model.randomizer.impl.SentenceRandomizerImpl;
-import model.tagger.PosTaggerManager;
-import model.tagger.PosTaggerResult;
+import model.tagger.PosTagger;
+import model.tagger.impl.PosTaggerImpl;
+import utility.Message;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FoundPoetryCLIToolApplication {
     private static final String MODEL_FILE_PATH = ModelFilePathConfig.getProperty("MODEL_FILE_PATH");
 
     public static void main(String[] args) {
-        PosTaggerResult posTaggerResult = new PosTaggerManager(MODEL_FILE_PATH).getResult();
-
-        if (!posTaggerResult.isSuccess()) {
-            System.out.println(posTaggerResult.errorMessage());
-            return;
+        PosTagger posTagger = null;
+        try {
+            posTagger = initPosTagger();
+        } catch (NullOrEmptyFilePathException e) {
+            System.out.println(Message.systemShutdownMessage());
+            System.err.println("PosTagger model file path is null or empty");
+        } catch (IncorrectFileFormatException e) {
+            System.out.println(Message.systemShutdownMessage());
+            System.err.println("PosTagger model file path is an incorrect file format. Expected .bin");
+        } catch (NonExistentFileException e) {
+            System.out.println(Message.systemShutdownMessage());
+            System.err.println("PosTagger model file does not exist");
+        } catch (PosTaggerIOException e) {
+            System.out.println(Message.systemShutdownMessage());
+            System.err.println("Failed to load PosTagger at: " + MODEL_FILE_PATH + e.getMessage());
         }
 
         DocumentLibrary documentLibrary = new DocumentLibraryImpl();
@@ -30,6 +48,29 @@ public class FoundPoetryCLIToolApplication {
         DocxDocumentCreator documentCreator = new DocxDocumentCreatorImpl();
         DocxCombinationWriter combinationWriter = new DocxCombinationWriterImpl();
 
-        new Application(posTaggerResult.posTagger(), documentLibrary, sentenceRandomizer, patternRandomizer, documentCreator, combinationWriter).run();
+        new Application(posTagger, documentLibrary, sentenceRandomizer, patternRandomizer, documentCreator, combinationWriter).run();
+    }
+
+    private static PosTagger initPosTagger() throws NullOrEmptyFilePathException,
+                                                    IncorrectFileFormatException,
+                                                    NonExistentFileException,
+                                                    PosTaggerIOException {
+
+        if (MODEL_FILE_PATH == null || MODEL_FILE_PATH.trim().isEmpty()) {
+            throw new NullOrEmptyFilePathException((MODEL_FILE_PATH));
+        }
+
+        final String expectedFormat = ".bin";
+        if (!MODEL_FILE_PATH.endsWith(expectedFormat)) {
+            throw new IncorrectFileFormatException(MODEL_FILE_PATH, expectedFormat);
+        }
+
+        if (!Files.exists(Paths.get(MODEL_FILE_PATH))) {
+            throw new NonExistentFileException(MODEL_FILE_PATH);
+        }
+
+        PosTagger posTagger = new PosTaggerImpl();
+        posTagger.initializeTagger(MODEL_FILE_PATH);
+        return posTagger;
     }
 }
