847571b3aefb71105a73c0cf59a15afe4d692ebc
dkwelcher <dkwelcher@gmail.com>
2024-10-28 17:27:59 -0400

    Added initializeTagger method & throws logic
    PosTagger exceptions condensed to reduce responsibility of class. Exceptions resulting from human error moved to calling class.

--- src/main/java/model/tagger/PosTagger.java
+++ src/main/java/model/tagger/PosTagger.java
@@ -1,5 +1,9 @@
 package model.tagger;
 
+import exception.PosTaggerIOException;
+import exception.TaggerNotInitializedException;
+
 public interface PosTagger {
-    String[] getTags(String[] tokens);
+    void initializeTagger(String filePath) throws PosTaggerIOException;
+    String[] getTags(String[] tokens) throws TaggerNotInitializedException;
 }
