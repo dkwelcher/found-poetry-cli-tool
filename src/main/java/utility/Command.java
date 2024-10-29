8f3b3640cd8dfe1cc2877447021227d062577475
dkwelcher <dkwelcher@gmail.com>
2024-10-29 18:26:40 -0400

    Impl static class for command variables

---
+++ src/main/java/utility/Command.java
@@ -0,0 +1,11 @@
+package utility;
+
+public final class Command {
+    private Command() {}
+
+    public static final String USER_SUBMIT_DOCUMENT_COMMAND = "1";
+    public static final String USER_SPECIFY_OUTPUT_FILE_PATH_COMMAND = "2";
+    public static final String USER_RANDOMIZE_BY_SENTENCE_COMMAND = "3";
+    public static final String USER_RANDOMIZE_BY_PATTERN_COMMAND = "4";
+    public static final String USER_EXIT_COMMAND = "5";
+}
